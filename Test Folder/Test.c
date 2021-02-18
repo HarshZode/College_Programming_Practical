f5ae8749b2a0332e2bbc05973ce46dc9f6a21652
Harsh Zode <zodeharsh@gmail.com>
2021-02-18 07:20:21 +0530

    This program determines which is the greatest number among the entered 4 numbers by the user

--- Test Folder/Test.c
+++ Test Folder/Test.c
@@ -1,21 +1,37 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
 
-    int a;
-    int b;
 
-    printf("Enter Number\n");
-    scanf("%d",&a);
+int main()
+{
+    // FIND THE GREATEST NUMBER
+    int a,b,c,d;
+    printf("Enter First Number\n");
+    scanf("%d", &a);
 
-    printf("Enter Number\n");
-    scanf("%d",&b);
+    printf("Enter Second Number\n");
+    scanf("%d", &b);
 
-    printf("The sum of entered number is %d.\n", a + b);
+    printf("Enter Third Number\n");
+    scanf("%d", &c);
+
+    printf("Enter Fourth Number\n");
+    scanf("%d", &d);
+
+    if (a>b && a>c && a>d){
+        printf("The Number %d is the greatest amongst other numbers.\n", a);
+    }
+    else if (b>a && b>c && b>d){
+        printf("The Number %d is the greatest amongst other numbers.\n", b);
+    }
+    else if (c>d &&c>b && c>a)
+    {
+        printf("The Number %d is the greatest amongst other numbers.\n", c);
+    }
+    else if (d>c && d>b &&d >a){
+        printf("The Number %d is the greatest amongst other numbers.\n", d);
+    }
 
     return 0;
-
-
-
-
 }
