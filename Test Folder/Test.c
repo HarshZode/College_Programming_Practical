8ed1d1ae6544baa7d7c3c5869725749f062027be
Harsh Zode <zodeharsh@gmail.com>
2021-02-18 07:14:01 +0530

    This is the file that can asks the user to enter 2 numbers to add.

---
+++ Test Folder/Test.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    float a;
+    float b;
+
+    printf("Enter Number\n");
+    scanf("%d",&a);
+
+    printf("Enter Number\n");
+    scanf("%d",&b);
+
+    printf("The sum of entered number is %d.\n", a + b);
+
+    return 0;
+
+
+
+
+}
