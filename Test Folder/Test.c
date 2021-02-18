defa6a44aa8e0ec421cd3eec058ae442eba67fef
Harsh Zode <zodeharsh@gmail.com>
2021-02-18 07:53:11 +0530

    Cleared the workspace for testing.c

--- Test Folder/Test.c
+++ Test Folder/Test.c
@@ -5,33 +5,6 @@
 
 int main()
 {
-    // FIND THE GREATEST NUMBER
-    int a,b,c,d;
-    printf("Enter First Number\n");
-    scanf("%d", &a);
-
-    printf("Enter Second Number\n");
-    scanf("%d", &b);
-
-    printf("Enter Third Number\n");
-    scanf("%d", &c);
-
-    printf("Enter Fourth Number\n");
-    scanf("%d", &d);
-
-    if (a>b && a>c && a>d){
-        printf("The Number %d is the greatest amongst other numbers.\n", a);
-    }
-    else if (b>a && b>c && b>d){
-        printf("The Number %d is the greatest amongst other numbers.\n", b);
-    }
-    else if (c>d &&c>b && c>a)
-    {
-        printf("The Number %d is the greatest amongst other numbers.\n", c);
-    }
-    else if (d>c && d>b &&d >a){
-        printf("The Number %d is the greatest amongst other numbers.\n", d);
-    }
 
     return 0;
 }
