1c45990eaf80391a295521c3a6c50ebdc7412ba7
Harsh Zode <zodeharsh@gmail.com>
2021-02-26 11:26:43 +0530

    Added comments in the program.

--- First Year/C Programming Examples/Increment Operators/Untitled1.c
+++ First Year/C Programming Examples/Increment Operators/Untitled1.c
@@ -7,9 +7,9 @@
 {
     int b=20,c;
     printf("\n the value of b is %d",b);
-    printf("\n the value of c is %d",c=--b);
+    printf("\n the value of c is %d",c=--b);  // this says first decrease then assign b to c
     printf("\n the value of b is %d",b);
-    printf("\n the value of c is %d",c=b--);
+    printf("\n the value of c is %d",c=b--);  // this says first assign b to c then decrease
 
     printf("\n the value of b is %d",b);
 
