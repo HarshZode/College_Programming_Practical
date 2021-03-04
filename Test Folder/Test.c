08b54b341a60b5027f0c6a4f35751bb1858bd0b3
Harsh Zode <zodeharsh@gmail.com>
2021-03-04 07:31:47 +0530

    Updated Test file.

--- Test Folder/Test.c
+++ Test Folder/Test.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-
-
-
- main()
-{
-    int b=20,c;
-    printf("\n the value of b is %d",b);
-    printf("\n the value of c is %d",c=--b);
-    printf("\n the value of b is %d",b);
-    printf("\n the value of c is %d",c=b--);
-
-    printf("\n the value of b is %d",b);
-
-
-}
